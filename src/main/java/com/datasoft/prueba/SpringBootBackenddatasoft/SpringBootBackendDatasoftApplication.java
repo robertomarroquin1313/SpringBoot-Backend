15ab38e0bcb1b94d5a27b8ad5d2a2679061093e6
Roberto1313 <67878026+Roberto1313@users.noreply.github.com>
2023-03-04 14:37:00 -0600

    primer commit

---
+++ src/main/java/com/datasoft/prueba/SpringBootBackenddatasoft/SpringBootBackendDatasoftApplication.java
@@ -0,0 +1,13 @@
+package com.datasoft.prueba.SpringBootBackenddatasoft;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBackendDatasoftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBackendDatasoftApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/datasoft/prueba/SpringBootBackenddatasoft/SpringBootBackendDatasoftApplicationTests.java
@@ -0,0 +1,13 @@
+package com.datasoft.prueba.SpringBootBackenddatasoft;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBackendDatasoftApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
